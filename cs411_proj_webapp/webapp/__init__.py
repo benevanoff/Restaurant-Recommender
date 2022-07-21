70a65422f8ebdbccd2969d48cb236e6f3506d05c
Ben Evanoff <benjaminevanoff99@gmail.com>
2022-07-21 14:11:08 -0500

    add db engine helper

--- cs411_proj_webapp/webapp/__init__.py
+++ cs411_proj_webapp/webapp/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from config import Config
+import sqlalchemy
+import os
 
+def init_db():
+    pool = sqlalchemy.create_engine(
+            sqlalchemy.engine.url.URL(
+                drivername="mysql+pymysql",
+                username= os.environ.get("DB_USER"),
+                password=os.environ.get("DB_PASSWD"),
+                database=os.environ.get("DB_NAME"),
+                host= os.environ.get("DB_HOST")
+            )
+        )
+    return pool
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = init_db()
 
 from webapp import routes
