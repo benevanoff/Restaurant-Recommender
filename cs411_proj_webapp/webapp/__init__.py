963fe347757cc10aafcc60efbb0d8b5d51d3e195
yanzhen4 <yanzhen4@illinois.edu>
2022-07-22 10:40:08 -0400

    connection to database

--- cs411_proj_webapp/webapp/__init__.py
+++ cs411_proj_webapp/webapp/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from config import Config
+from yaml import load, Loader
 import sqlalchemy
 import os
 
 def init_db():
+    if os.environ.get('GAE_ENV') != 'standard':
+        variables = load(open("app.yaml"), Loader = Loader)
+        env_variables = variables['env_variables']
+        for var in env_variables:
+            os.environ[var] = env_variables[var]
+
     pool = sqlalchemy.create_engine(
             sqlalchemy.engine.url.URL(
                 drivername="mysql+pymysql",
-                username= os.environ.get("DB_USER"),
-                password=os.environ.get("DB_PASSWD"),
-                database=os.environ.get("DB_NAME"),
-                host= os.environ.get("DB_HOST")
+                username= os.environ.get("MYSQL_USER"),
+                password=os.environ.get("MYSQL_PASSWORD"),
+                database=os.environ.get("MYSQL_NAME"),
+                host= os.environ.get("MYSQL_HOST")
             )
         )
     return pool
@@ -18,5 +25,10 @@ def init_db():
 app = Flask(__name__)
 app.config.from_object(Config)
 db = init_db()
+
+conn = db.connect()
+results = conn.execute("SELECT * FROM cs411_proj_data.Bar").fetchall()
+print([x for x in results])
+conn.close()
 
 from webapp import routes
